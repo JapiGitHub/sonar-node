590aec4d431ae6ffd9f942ad90f3adbdeceba5ac
json-voorhees-bourne <janne.korkee@gmail.com>
2022-02-05 21:53:38 +0200

    kokonaan uusiksi koko setti

---
+++ feature.js
@@ -0,0 +1,9 @@
+const feature = function () {
+  const password = "p455w0rd";
+
+  console.log(password);
+  return password;
+};
+feature();
+
+module.exports = feature;
---
+++ feature.test.js
@@ -0,0 +1,5 @@
+const feature = require("./feature");
+
+test("easy test", () => {
+  expect(feature()).toBe("p455w0rd");
+});
