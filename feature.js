c5f459018dfbeb20f79889d32689ac5923258722
json-voorhees-bourne <janne.korkee@gmail.com>
2022-02-06 17:26:44 +0200

    renew test and infinite for loop

--- feature.js
+++ feature.js
@@ -1,3 +1,4 @@
+// as you can see, sonarLint doesn't recognize taint analysis! only sonarQube can see this!
 const feature = function () {
   const password = "p455w0rd";
 
@@ -6,4 +7,11 @@ const feature = function () {
 };
 feature();
 
+// infinite loop!
+var j = 0;
+while (true) {
+  j++;
+}
+
+//because of infinite loop this code is actually never reached!
 module.exports = feature;
--- feature.test.js
+++ feature.test.js
@@ -1,5 +1,5 @@
 const feature = require("./feature");
 
-test("easy test", () => {
+test("tests that password is hardcoded. dont really do this!", () => {
   expect(feature()).toBe("p455w0rd");
 });
